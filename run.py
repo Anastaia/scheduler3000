cdc7fdcd51f50163268ad0b7e1e4978ac9ef805a
Don Macaron <donmacaron95@gmail.com>
2020-08-15 08:26:55 +0300

    db integration

--- run.py
+++ run.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f106a85651d73990e22af513b9dbfa24'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-group_list = {
-        '11-312': '312.pdf',
-        '11-666': '666.pdf',
-        '11-1337': '1337.pdf',
-        '11-69': '69.pdf',
-    }
+# Database Model
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group_number = db.Column(db.String(6), unique=True, nullable=False)
+    file_name = db.Column(db.String(10), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Group('{self.group_number}')"
 
 
+# Creating dict from db data
+def group_list():
+    return {item.group_number: item.file_name for item in Group.query.all()}
+
+
+# Route for home page
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -25,7 +38,7 @@ def timetable_page(group):
 
 @app.context_processor
 def context_processor():
-    return dict(group_list=[*group_list])
+    return dict(group_list=[*group_list()])
 
 
 if __name__ == '__main__':
